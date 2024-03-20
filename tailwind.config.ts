89dee8d7eeea424780c4f3b89dc36b80441c5358
kelvinbush <kelybush@gmail.com>
2024-03-20 11:07:08 +0300

    set up custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -19,6 +19,42 @@ const config = {
     },
     extend: {
       colors: {
+        "blue-gray": {
+          "50": "#e9ebeb",
+          "100": "#bcbfc1",
+          "200": "#9ba1a3",
+          "300": "#6e767a",
+          "400": "#515b60",
+          "500": "#263238",
+          "600": "#232e33",
+          "700": "#1b2428",
+          "800": "#151c1f",
+          "900": "#101518",
+        },
+        blue: {
+          "50": "#eef3f7",
+          "100": "#cadae5",
+          "200": "#b0c9d9",
+          "300": "#8cb0c7",
+          "400": "#75a1bd",
+          "500": "#5389ac",
+          "600": "#4c7d9d",
+          "700": "#3b617a",
+          "800": "#2e4b5f",
+          "900": "#233a48",
+        },
+        teal: {
+          "50": "#e6f5f3",
+          "100": "#b0deda",
+          "200": "#8acfc8",
+          "300": "#54b9af",
+          "400": "#33aba0",
+          "500": "#009688",
+          "600": "#00897c",
+          "700": "#006b61",
+          "800": "#00534b",
+          "900": "#003f39",
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -75,6 +111,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
